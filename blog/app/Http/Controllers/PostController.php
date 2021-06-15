c86f68548620cad3bffaa3c7bb754c74bff9306f
connte0745 <ryoya15662@gmail.com>
2021-06-15 18:23:11 +0900

    blog

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -10,10 +10,10 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        $datas = $post->latest()->get();
+        //$datas = $post->latest()->get();
         // https://blog.capilano-fw.com/?p=665#latest
         
-        return view('index',compact('datas'));
+        return view('index')->with(['datas' => $post ->getPaginateByLimit(5)]);
         // https://qiita.com/ryo2132/items/63ced19601b3fa30e6de
     }
     
--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -4,29 +4,32 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="description" content="simple_blog" />
-
         <title>blog_view</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     
     <body>
         <h1>blog</h1>
         <p>this is a sample blog</p>
-        <table border="1" style="border-collapse: collapse">
-            <tr>
-                <td>title</td>
-                <td>contents</td>
-            </tr>
-            
-            @foreach($datas as $data)
+        
+        <div class = 'posts'>
+            <table border="1" style="border-collapse: collapse">
                 <tr>
-                    <th>{{ $data->title }}</th>
-                    <th>{{ $data->body }}</th>
+                    <td>title</td>
+                    <td>contents</td>
                 </tr>
-                @if ($loop -> index == 10)
-                    @break
-                @endif
-            @endforeach
-        </table>
+                
+                @foreach($datas as $data)
+                    <tr>
+                        <th>{{ $data->title }}</th>
+                        <th>{{ $data->body }}</th>
+                    </tr>
+                @endforeach
+                <div class = 'pagenate'>
+                    {{ $datas -> links()}}
+                </div>
+            </table>
+        </div>
         
        
     </body>
