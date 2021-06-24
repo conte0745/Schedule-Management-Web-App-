fcb3934b48e5f6690cfb9aef6cd5920b1487e74a
connte0745 <ryoya15662@gmail.com>
2021-06-24 18:25:21 +0900

    delete

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -22,13 +22,21 @@
                     <td class = "text-center">title</td>
                     <td class = "text-center">contents</td>
                     <td class = "text-center">task</td>
+                    <td class = "text-center">task</td>
+                   
                 </tr>
                 
                 @foreach($datas as $data)
                     <tr>
                         <th><a href="/posts/{{ $data->id}}">{{ $data->title }}</a></th>
                         <th><a href="/posts/{{ $data->id}}">{{ $data->body }}</a></th>
-                        <th><a href="/posts/{{ $data->id}}/edit">edit</a></th>
+                        <th><a href="/posts/{{ $data->id }}/edit" class="btn btn-primary btn-sm">edit</a></th>
+                        <th><form action="/posts/delete/{{$data->id}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell" onclick="return confirm('削除しますか？')">
+                            </form>
+                        </th>
                     </tr>
                 @endforeach
                 <div class = 'pagenate'>
