a263b9581c422174dc277082e240cce4e886930f
connte0745 <ryoya15662@gmail.com>
2021-06-25 21:40:23 +0900

    navi

--- blog/resources/views/edit.blade.php
+++ blog/resources/views/edit.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.standard')
 
-        <title>simple blog detail</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    </head>
-    <body style = "background-color:#fffafa">
-        <form action="/posts/{{ $data->id }}" method="post">
-            @csrf
-            @method('PUT')
+@section('contains') 
+    <form action="/posts/{{ $data->id }}" method="post">
+        @csrf
+        @method('PUT')
     	        <p>タイトル：<br>
         	<input type="text" name="post[title]" value="{{ $data->title }}" placeholder="title" class="@error('post.title') is-invalid @enderror"></p>
     	    @error('post.title')
@@ -26,6 +18,4 @@
         </form>
         
         <p><a href="/posts">[[return]]</a></p>
-    </body>
-    </body>
-</html>
+@endsection
--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
+@extends('layouts.standard')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="description" content="simple_blog" />
-        <title>blog_view</title>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    
-    <body style="background-color:#fffafa">
+@section('contains')    
         <h1>blog</h1>
         <p>this is a sample blog</p>
         
         <div class = "contains">
             
-            <a href="posts/create">新規作成</a>
+            <a href="/posts/create">新規作成</a>
             <table class = "table table-bordered">
                 <tr>
                     <td class = "text-center">title</td>
@@ -45,7 +35,5 @@
             </table>
         </div>
         
-       
-    </body>
-    
-</html>
+    @endsection
+ 
